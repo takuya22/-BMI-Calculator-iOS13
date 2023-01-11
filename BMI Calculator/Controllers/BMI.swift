781ed9a00a560c3ae67e3508a0f176643d80f609
Takuya Yanagawa <takuya@MACUser-no-MacBook-Air.local>
2023-01-12 00:37:13 +0900

    BMI Calculator 作成

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Takuya Yanagawa on 2023/01/12.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
